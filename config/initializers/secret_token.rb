428f50e7c0b390c07e3599b6b73baad092978e9a
Leo Martin Villasica <exst_lvillasica@yahoo.com>
2013-02-02 21:24:35 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeesamBooking::Application.config.secret_token = 'd0c30d4f100b622d268773b616b6b44d6b74434708b1a8d1bd9a739ff8ade1c26e2a436a27009c851714e1f0c5884ed104c04e49f96d6ae816249ed9c460fcb9'
